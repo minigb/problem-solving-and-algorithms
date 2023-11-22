59879fa541bec7df2a9692d2b665954c67d56e98
Minhee Lee <76970075+minigb@users.noreply.github.com>
2023-11-23 01:05:30 +0900

    Upload solution for Problem #5086.

---
+++ Baekjoon-Online-Judge/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <iomanip>
+using namespace std;
+typedef long long ll;
+const int int_inf = 2147483646;
+const ll ll_inf = 9223372036854775806;
+#define PI 3.141592653589793238462643383279502
+//소수점 출력
+//cout << fixed << setprecision(10) << ans << '\n'. setprecision은 iomanip 라이브러리에 있음
+//공백 포함해서 문자열 받기: getline(cin, s);
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int a, b;
+
+	while (1) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) {
+			break;
+		}
+
+		if (b % a == 0) {
+			cout << "factor" << '\n';
+		}
+		else if (a % b == 0) {
+			cout << "multiple" << '\n';
+		}
+		else {
+			cout << "neither" << '\n';
+		}
+	}
+
+	return 0;
+}
