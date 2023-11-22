b96217cb3f9e1dcee7b105dc1255a92e1ef8fb6f
Minhee Lee <76970075+minigb@users.noreply.github.com>
2023-11-23 01:03:44 +0900

    Upload solution for Problem #2869.

---
+++ Baekjoon-Online-Judge/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iomanip>
+using namespace std;
+typedef long long ll;
+const int int_inf = 2147483646;
+const ll ll_inf = 9223372036854775806;
+#define PI 3.141592653589793238462643383279502
+//소수점 출력
+//cout << fixed << setprecision(10) << ans << '\n'. setprecision은 iomanip 라이브러리에 있음
+//공백 포함해서 문자열 받기: getline(cin, s);
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int A, B, V;
+	
+	cin >> A >> B >> V;
+	cout << ((V - A) + (A - B - 1)) / (A - B) + 1 << '\n';
+
+	return 0;
+}
