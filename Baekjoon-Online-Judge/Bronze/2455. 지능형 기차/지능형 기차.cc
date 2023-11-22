13d344a0343249877401a9cbb835ed700025fbaf
Minhee Lee <76970075+minigb@users.noreply.github.com>
2023-11-23 01:02:32 +0900

    Upload solution for Problem #2455.

---
+++ Baekjoon-Online-Judge/Bronze/2455. 지능형 기차/지능형 기차.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <iomanip>
+using namespace std;
+typedef long long ll;
+const int int_inf = 2147483646;
+const ll ll_inf = 9223372036854775806;
+#define PI 3.141592653589793238462643383279502
+//소수점 출력
+//cout << fixed << setprecision(10) << ans << '\n'. setprecision은 iomanip 라이브러리에 있음
+//공백 포함해서 문자열 받기: getline(cin, s);
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int T;
+	int a, b, cur;
+	int max_ = 0;
+
+	T = 4;
+	cur = 0;
+	while (T--) {
+		cin >> a >> b;
+		cur = cur - a + b;
+		max_ = max(max_, cur);
+	}
+
+	cout << max_ << '\n';
+
+	return 0;
+}
