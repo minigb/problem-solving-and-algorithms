2cbefa2ce8c801e7b4cff759124c7ad9036b5ca8
Minhee Lee <76970075+minigb@users.noreply.github.com>
2023-11-23 01:07:48 +0900

    Upload solution for Problem #5565.

---
+++ Baekjoon-Online-Judge/Bronze/5565. 영수증/영수증.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <iomanip>
+using namespace std;
+typedef long long ll;
+const int int_inf = 2147483646;
+const ll ll_inf = 9223372036854775806;
+#define PI 3.141592653589793238462643383279502
+//소수점 출력
+//cout << fixed << setprecision(10) << ans << '\n'. setprecision은 iomanip 라이브러리에 있음
+//공백 포함해서 문자열 받기: getline(cin, s);
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int sum;
+	int temp;
+	int i;
+
+	cin >> sum;
+	for (i = 0; i < 9; i++) {
+		cin >> temp;
+		sum -= temp;
+	}
+
+	cout << sum << '\n';
+
+	return 0;
+}
