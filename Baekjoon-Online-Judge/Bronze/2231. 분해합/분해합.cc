717b2c3896420a102a1af25b0dc50d9b08d7047f
Minhee Lee <76970075+minigb@users.noreply.github.com>
2023-11-23 01:21:04 +0900

    Upload solution for Problem #2231.

---
+++ Baekjoon-Online-Judge/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iomanip>
+using namespace std;
+typedef long long ll;
+const int int_inf = 2147483646;
+const ll ll_inf = 9223372036854775806;
+#define PI 3.141592653589793238462643383279502
+//소수점 출력
+//cout << fixed << setprecision(10) << ans << '\n'. setprecision은 iomanip 라이브러리에 있음
+//공백 포함해서 문자열 받기: getline(cin, s);
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int N;
+	int num;
+	int sum;
+	int temp;
+
+	cin >> N;
+	for (num = 1; num <= N; num++) {
+		temp = num;
+		sum = num;
+		while (temp) {
+			sum += temp % 10;
+			temp /= 10;
+		}
+		if (sum == N) {
+			break;
+		}
+	}
+
+	if (num <= N) {
+		cout << num << '\n';
+	}
+	else {
+		cout << 0 << '\n';
+	}
+
+	return 0;
+}
